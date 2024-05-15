85d890a06c5cd9aed7da6da8e55f6d130a5566fc
rk <this.rajkhare@gmail.com>
2024-05-14 19:04:58 -0700

    more efficient math

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,14 +2,8 @@ export function formatLargeNumber(num: number): string {
     if (num < 1000) return num.toString();
 
     const units = ["K", "M", "G", "T", "P", "E"];
-    let unitIndex = -1;
-    let reducedNum = num;
+    const unitIndex = Math.min(Math.floor(Math.log10(num) / 3), units.length - 1);
+    const reducedNum = num / Math.pow(1000, unitIndex);
 
-    while (reducedNum >= 1000 && unitIndex < units.length - 1) {
-        reducedNum /= 1000;
-        unitIndex++;
-    }
-
-    // Use toFixed to control decimal places (e.g., 1.2K instead of 1.234K)
     return reducedNum.toFixed(1) + units[unitIndex];
 }

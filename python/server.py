52ba5ce117469d85c95e00c64a6bd8bae7203b24
Lienid <0xLienid@protonmail.com>
2024-05-21 10:42:30 -0700

    remove db creation for now

--- python/server.py
+++ python/server.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI, WebSocket
 from starlette.websockets import WebSocketDisconnect
 from loguru import logger
-from DownloadManager import DownloadManager
-from db_manager import create_models_table
 
 app = FastAPI()
 
@@ -38,6 +36,4 @@ async def delete_model(model_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-
-    create_models_table()
     uvicorn.run(app, host="0.0.0.0", port=8899)

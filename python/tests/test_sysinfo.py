87ffe738511a335ef8311c498785a7151245a071
rk <this.rajkhare@gmail.com>
2024-05-21 14:17:19 -0700

    typo

--- python/tests/test_sysinfo.py
+++ python/tests/test_sysinfo.py
@@ -85,7 +85,7 @@ async def test_ram_change_detection():
     stress_process = subprocess.Popen(['stress-ng', '--vm', '1', '--vm-bytes', '4G', '--timeout', '5s'])
 
     # Wait for stress-ng to start affecting RAM
-    await asyncio.sleep(5)  # Adjust time as necessary for your system
+    await asyncio.sleep(5)  
 
     # Get updated data
     updated_data = next(generator)

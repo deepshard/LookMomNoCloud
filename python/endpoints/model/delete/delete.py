a1ee8925df9b65a65e10cd87226a3355a1d8602c
Lienid <0xLienid@protonmail.com>
2024-05-23 10:37:46 -0700

    Logging

--- python/endpoints/model/delete/delete.py
+++ python/endpoints/model/delete/delete.py
@@ -1,7 +1,9 @@
 import shutil
+from loguru import logger
 from python.utils import get_app_data_path
 
 
 def delete_model_handler(model_id: str):
     model_path = get_app_data_path() / "models" / model_id
+    logger.info(f"Deleting model at: {model_path}")
     shutil.rmtree(model_path)

56caec8ac025f57ed88dac482139fcbefd877ef7
rk <this.rajkhare@gmail.com>
2024-05-27 13:54:47 -0700

    fix

--- python/utils.py
+++ python/utils.py
@@ -47,32 +47,31 @@ def get_quantization_compression(quant: Quantization) -> float:
     quantization_compression_table = {
         Quantization.INT3: 0.25,
         Quantization.INT4: 0.33,
-        Quantization.INT8: 0.55
+        Quantization.INT8: 0.55,
     }
     return quantization_compression_table[quant]
 
 
 def is_convertable_format(base_weights_path: str) -> bool:
-    pytorch_json_path = os.path.join(
-        base_weights_path, "pytorch_model.bin.index.json")
+    pytorch_json_path = os.path.join(base_weights_path, "pytorch_model.bin.index.json")
     pytorch_bin_path = os.path.join(base_weights_path, "pytorch_model.bin")
-    safetensors_path = os.path.join(
-        base_weights_path, "model.safetensors.index.json")
-    safetensors_bin_path = os.path.join(
-        base_weights_path, "model.safetensors")
+    safetensors_path = os.path.join(base_weights_path, "model.safetensors.index.json")
+    safetensors_bin_path = os.path.join(base_weights_path, "model.safetensors")
 
     if (
-        os.path.exists(pytorch_json_path) or
-        os.path.exists(pytorch_bin_path) or
-        os.path.exists(safetensors_path) or
-        os.path.exists(safetensors_bin_path)
+        os.path.exists(pytorch_json_path)
+        or os.path.exists(pytorch_bin_path)
+        or os.path.exists(safetensors_path)
+        or os.path.exists(safetensors_bin_path)
     ):
         return True
 
     return False
 
 
-def get_model_size_info(weights_path: str, quantization: Quantization) -> tuple[int, float]:
+def get_model_size_info(
+    weights_path: str, quantization: Quantization
+) -> tuple[int, float]:
     model_size = get_disk_usage(weights_path)
     compression_rate = get_quantization_compression(quantization)
     compressed_size = model_size * compression_rate

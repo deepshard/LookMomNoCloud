8d8a762c065f3aed7a474b1682f253283fd7d7de
rk <this.rajkhare@gmail.com>
2024-05-27 17:20:25 -0700

    bug

--- python/db.py
+++ python/db.py
@@ -4,13 +4,13 @@ from utils import get_app_data_path
 
 db: Prisma = None
 
-file = os.getenv(
+db_path = os.getenv(
     "DATABASE_URL",
     (
-        str(get_app_data_path() / "truffle.db")
+        f"file:{str(get_app_data_path() / 'truffle.db')}"
         if os.getenv("ENV") == "prod"
-        else str(get_app_data_path() / "truffle.test.db")
+        else f"file:{str(get_app_data_path() / 'truffle.test.db')}"
     ),
 )
 
-db = Prisma(datasource={"provider": "sqlite", "url": f"file:{file}"})
+db = Prisma(datasource={"provider": "sqlite", "url": f"{db_path}"})

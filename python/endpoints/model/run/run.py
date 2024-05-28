d42cf942df3e11d5569f1a32c02696d3e2dc0050
Lienid <0xLienid@protonmail.com>
2024-05-27 17:38:51 -0700

    lint

--- python/endpoints/model/run/run.py
+++ python/endpoints/model/run/run.py
@@ -254,8 +254,7 @@ async def run_models_generator(
         available_ram = get_usable_memory()
         if model_size > available_ram:
             logger.error(
-                f"Not enough memory to convert and quantize the model {
-                    model_id}"
+                f"Not enough memory to convert and quantize the model {model_id}"
             )
             error_event = {
                 "id": model_id,

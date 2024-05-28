cc454b92fa58998b68890429d776a2009bc234ad
rk <this.rajkhare@gmail.com>
2024-05-28 11:46:44 -0700

    fix

--- python/tests/test_sysinfo.py
+++ python/tests/test_sysinfo.py
@@ -24,7 +24,7 @@ async def test_ram_change_detection():
 
         # Start stress-ng to use RAM
         stress_process = subprocess.Popen(
-            ["stress-ng", "--vm", "1", "--vm-bytes", "4G", "--timeout", "5s"]
+            ["stress-ng", "--vm", "1", "--vm-bytes", "5G", "--timeout", "5s"]
         )
 
         # Wait for stress-ng to start affecting RAM

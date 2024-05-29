4ae335de5e49b0a82d4075e0af0d529248986ab7
Raj Khare <37476411+raj-khare@users.noreply.github.com>
2024-05-29 11:22:27 -0700

    tests cicd (#26)
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * bugfix
    
    * bug
    
    * bug
    
    * fix
    
    * fix
    
    * timeout
    
    * bruh
    
    * Matt/cicd tests (#25)
    
    * Try fix
    
    * Try fix
    
    * remove unnecessary changes
    
    * fix
    
    * bugfix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * push
    
    * fix
    
    * fix
    
    * fix
    
    * format
    
    * fix
    
    ---------
    
    Co-authored-by: 0xLienid <92545857+0xLienid@users.noreply.github.com>

--- python/truffle_types.py
+++ python/truffle_types.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from enum import Enum
+from typing import List, Dict, Literal
+from uuid import UUID
 
 
 class RepoType(Enum):
@@ -27,6 +29,31 @@ class FileInfo:
 
 
 @dataclass
+class SystemResourceDetails:
+    ram: int
+    disk: int
+
+
+@dataclass
+class ModelResourceDetails:
+    id: UUID
+    disk: int
+    ram: int
+
+
+@dataclass
+class SystemResources:
+    available: SystemResourceDetails
+    models: List[ModelResourceDetails]
+    total: SystemResourceDetails
+
+
+@dataclass
+class SystemInfo:
+    os: Literal["MAC", "LINUX"]
+    resources: SystemResources
+
+
 class Model:
     id: str
     url: str

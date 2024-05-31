d554b5b9ab42c706965860cbb8fa432bdc2cdb3a
rk <this.rajkhare@gmail.com>
2024-05-30 18:00:48 -0700

    lint

--- src/utils/sysUtils.ts
+++ src/utils/sysUtils.ts
@@ -8,7 +8,7 @@ export function bytesToHumanReadable(bytes?: number, withUnit = true): string {
   const exponent = bytes > 0 ? Math.floor(Math.log(bytes) / Math.log(1024)) : 0;
   const result = (bytes / Math.pow(1024, exponent)).toFixed(2);
   return withUnit ? `${result} ${units[exponent]}` : result;
-};
+}
 
 
 interface PidStats {

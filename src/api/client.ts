56c05ea2ce87a684d862edf0f2735b71b52eaee7
rk <this.rajkhare@gmail.com>
2024-06-04 14:40:26 -0700

    fix

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,14 +1,30 @@
-import axios, { AxiosInstance } from 'axios'
+import axios, { AxiosInstance, AxiosProgressEvent } from 'axios'
 
 export const ROOTURL = 'http://0.0.0.0:8899';
 
 export class ApiClient {
   client: AxiosInstance
-  constructor(basePath: string) {
+  constructor(basePath: string, onProgressCallback?: (data: any) => void) {
     this.client = axios.create({
       baseURL: `${ROOTURL}/${basePath}` ?? '/',
       responseType: 'json',
-      headers: {}
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      onDownloadProgress: (progressEvent: AxiosProgressEvent) => {
+        if (onProgressCallback) {
+          const xhr = progressEvent.event.target
+          const { responseText } = xhr
+          try {
+            let data = responseText.split("data: ")[1]
+            data = JSON.parse(data)
+            onProgressCallback(data)
+          } catch (e) {
+            console.error("Error parsing progress event", e)
+          }
+        }
+      }
     });
   }
 
@@ -16,9 +32,14 @@ export class ApiClient {
     const response = await this.client.get(url);
     return response.data;
   }
+
+  post = async (url: string, data: any) => {
+    const response = await this.client.post(url, JSON.stringify(data));
+    return response.data;
+  }
 }
 
-const client = new ApiClient("")
+const defaultClient = new ApiClient("")
 
-export default client
+export default defaultClient
 
--- src/component/Highlights.tsx
+++ src/component/Highlights.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import useSWR from 'swr';
-import ApiClient from "../api/client";
+import defaultClient, { ApiClient } from "../api/client";
+import axios from "axios";
 
 interface Highlight {
     id: string;
@@ -15,8 +16,26 @@ interface Highlight {
     progress: number;
 }
 
+const Status = ({ status }: { status: Highlight['status'] }) => {
+    async function installModel() {
+        const api = new ApiClient("model", (data) => {
+            console.log(data)
+        })
+        await api.post("/install", {
+            id: "5ae0070b-e52e-4605-a356-de23c492203e",
+            url: "https://huggingface.co/meta-llama/Meta-Llama-3-8B"
+        })
+    }
+
+    if (status === "NOT_DOWNLOADED") return <button className="relative" onClick={installModel}>INSTALL</button>;
+    if (status === "DOWNLOADING") return <div className="relative">DOWNLOADING</div>;
+    if (status === "INSTALLING") return <div className="relative">INSTALLING</div>;
+    if (status === "RUNNING") return <div className="relative">RUNNING</div>;
+    if (status === "STOPPED") return <div className="relative">STOPPED</div>;
+};
+
 export default function Highlights() {
-    const { data, error } = useSWR<Highlight[]>('/highlights', ApiClient.get);
+    const { data, error } = useSWR<Highlight[]>('/highlights', defaultClient.get);
 
     if (error) return <div>Failed to load</div>;
     if (!data) return <div>Loading...</div>;
@@ -27,7 +46,7 @@ export default function Highlights() {
                 <div key={highlight.id} className="relative w-32 h-20 rounded-sm overflow-hidden">
                     <img src={highlight.background_image} alt={highlight.name} className="absolute w-full h-full object-cover" />
                     <div className="relative">{highlight.name}</div>
-                    <div className="relative">{highlight.status}</div>
+                    <div className="relative"><Status status={highlight.status} /></div>
                     <div className="relative">{highlight.progress}</div>
                 </div>
             ))}

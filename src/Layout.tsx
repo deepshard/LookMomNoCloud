6401caf732f0beac7ef11442255b5976b53239e5
Nacho-Giorgis <nacho@getswype.io>
2024-06-04 11:23:36 -0700

    removed classNames

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -5,10 +5,10 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div>
-      <section className="">
+      <section>
         <NavBar />
       </section>
-      <section className="">
+      <section>
         <Outlet />
       </section>
     </div>

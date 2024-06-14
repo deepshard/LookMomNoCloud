53899f16ee55f029c62a2fd3e9bf83a36f5b29cb
rk <this.rajkhare@gmail.com>
2024-06-14 01:22:29 -0700

    try?

--- python/alembic/env.py
+++ python/alembic/env.py
@@ -4,7 +4,8 @@ from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-from models import Base
+
+# from models import Base
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -20,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:

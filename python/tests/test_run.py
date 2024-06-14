fe182d12d6682e47a0f4ea08048690d744c57953
Lienid <0xLienid@protonmail.com>
2024-06-14 14:18:22 -0700

    speed up quant decision

--- python/tests/test_run.py
+++ python/tests/test_run.py
@@ -328,6 +328,7 @@ async def test_run_not_enough_space(session_fixture, mock_mlc, model_weights, mo
     # Mocks
     session_fixture("endpoints.model.run.run")
     mocker.patch("state.ModelManager.get_usable_memory", return_value=8192)
+    mocker.patch("state.ModelManager.ModelManager.get_expected_disk_consumption", return_value=0)
     mocker.patch("endpoints.model.run.run.get_usable_memory", return_value=8192)
     mocker.patch("psutil.disk_usage", return_value=MagicMock(free=1024))
 

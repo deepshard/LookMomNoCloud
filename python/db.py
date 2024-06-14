8704b67f94656da7d985caf6f724962b8b4872d9
Lienid <0xLienid@protonmail.com>
2024-06-14 11:04:53 -0700

    no -s

--- python/db.py
+++ python/db.py
@@ -5,5 +5,6 @@ from sqlalchemy.orm import sessionmaker
 from utils import get_db_path
 
 
-engine = create_async_engine(get_db_path(), echo=True)
-get_db_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+engine = create_async_engine(get_db_path(), echo=False)
+get_db_session = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession)

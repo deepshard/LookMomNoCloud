d6698c3099bc07fc72db68b2e7c3b84ce0ba9b60
Lienid <0xLienid@protonmail.com>
2024-06-14 11:09:54 -0700

    lint

--- python/db.py
+++ python/db.py
@@ -6,5 +6,4 @@ from utils import get_db_path
 
 
 engine = create_async_engine(get_db_path(), echo=False)
-get_db_session = sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession)
+get_db_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)

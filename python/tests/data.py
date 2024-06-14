4f2fe0599f26e3ace62957727a09ec0096b196bb
Lienid <0xLienid@protonmail.com>
2024-06-14 00:34:32 -0700

    push up conftest changes

--- python/tests/data.py
+++ python/tests/data.py
@@ -1,168 +1,121 @@
+import sys
+import pytest
+import pytest_asyncio
 import os
+import shutil
+import asyncio
+from pathlib import Path
+import tests.data as data
+from unittest import mock
+from unittest.mock import MagicMock, patch
+from aioresponses import aioresponses
+from db import get_db_session
+from state import global_state_manager
+from server import init_state
+from constants import TRUFFLE_API_URL
+from utils import get_app_data_path
+from models import RunningModel
+
+# Helpers
 
 
-# IDs
-ID = "ed8aee0b-8428-4c2f-9b8f-756c19ab57e9"
-ID_2 = "b438d015-ad45-4e9a-9aba-2e290348b078"
+def clear_path():
+    if os.path.exists("/tmp/models"):
+        shutil.rmtree("/tmp/models")
 
-# Request URLs
-MODEL_URL = "https://huggingface.co/openai-community/gpt2"
-HF_API_URL = "https://huggingface.co/api/models/openai-community/gpt2?"
-HF_API_URL_1 = "https://huggingface.co/api/models/1?"
-HF_API_URL_2 = "https://huggingface.co/api/models/2?"
 
-FILE_ONE_URL = "https://huggingface.co/openai-community/gpt2/resolve/main/pytorch_model.bin"
-FILE_TWO_URL = "https://huggingface.co/openai-community/gpt2/resolve/main/config.json"
-FILE_THREE_URL = "https://huggingface.co/openai-community/gpt2/resolve/main/onnx/onnx_model.onnx"
-FILE_FOUR_URL = "https://huggingface.co/openai-community/gpt2/resolve/main/tf_model/tf_model.pb"
+async def clear_db():
+    await RunningModel.delete_all()
 
-# Mock models
-MOCK_MODEL_1 = {
-    "id": ID,
-    "name": "test",
-    "title": "test",
-    "size": 1,
-    "author": "test",
-    "downloads": 1,
-    "likes": 1,
-    "intro": "test",
-    "capabilities": "test",
-    "risks": "test",
-    "hfLink": HF_API_URL,
-    "evalId": "test",
-    "bg_image_url": "test",
-}
-MOCK_MODEL_2 = {
-    "id": ID_2,
-    "name": "test",
-    "title": "test",
-    "size": 1,
-    "author": "test",
-    "downloads": 1,
-    "likes": 1,
-    "intro": "test",
-    "capabilities": "test",
-    "risks": "test",
-    "hfLink": HF_API_URL,
-    "evalId": "test",
-    "bg_image_url": "test",
-}
 
-# Mock API responses
-MOCK_API_RESPONSE = {
-    "siblings": [
-        {
-            "rfilename": "pytorch_model.bin",
-        },
-        {
-            "rfilename": "config.json",
-        },
-        {
-            "rfilename": "onnx/onnx_model.onnx",
-        },
-        {
-            "rfilename": "tf_model/tf_model.pb",
-        },
-    ]
-}
-mocked_response_1 = [
-    {"rfilename": "pytorch_model.bin"},
-    {"rfilename": "config.json"},
-    {"rfilename": "onnx/onnx_model.onnx"},
-    {"rfilename": "tf_model/tf_model.tflite"},
-    {"rfilename": "test.msgpack"},
-    {"rfilename": "test.h5"},
-]
-mocked_response_2 = [{"rfilename": "xyz.safetensors"}, {"rfilename": "consolidated.safetensors"}]
-MODELS = [
-    {
-        "id": "3fec7228-04de-485d-9f09-bde6e8ea350f",
-        "name": "test",
-        "title": "test",
-        "size": 1,
-        "author": "test",
-        "downloads": 1,
-        "likes": 1,
-        "intro": "test",
-        "capabilities": "test",
-        "risks": "test",
-        "hfLink": "test",
-        "evalId": "test",
-        "bg_image_url": "test",
-    },
-    {
-        "id": "b438d015-ad45-4e9a-9aba-2e290348b078",
-        "name": "test",
-        "title": "test",
-        "size": 1,
-        "author": "test",
-        "downloads": 1,
-        "likes": 1,
-        "intro": "test",
-        "capabilities": "test",
-        "risks": "test",
-        "hfLink": "test",
-        "evalId": "test",
-        "bg_image_url": "test",
-    },
-    {
-        "id": "e1b7a151-ad5a-4929-b9a2-20e42f629c4c",
-        "name": "test",
-        "title": "test",
-        "size": 1,
-        "author": "test",
-        "downloads": 1,
-        "likes": 1,
-        "intro": "test",
-        "capabilities": "test",
-        "risks": "test",
-        "hfLink": "test",
-        "evalId": "test",
-        "bg_image_url": "test",
-    },
-    {
-        "id": "c081e038-a74c-4a7d-87d6-f36bbf7ff373",
-        "name": "test",
-        "title": "test",
-        "size": 1,
-        "author": "test",
-        "downloads": 1,
-        "likes": 1,
-        "intro": "test",
-        "capabilities": "test",
-        "risks": "test",
-        "hfLink": "test",
-        "evalId": "test",
-        "bg_image_url": "test",
-    },
-    {
-        "id": "da05e829-9e9b-43d8-8c26-6141318700cb",
-        "name": "test",
-        "title": "test",
-        "size": 1,
-        "author": "test",
-        "downloads": 1,
-        "likes": 1,
-        "intro": "test",
-        "capabilities": "test",
-        "risks": "test",
-        "hfLink": "test",
-        "evalId": "test",
-        "bg_image_url": "test",
-    },
-]
+# Fixtures
+@pytest_asyncio.fixture(autouse=True)
+async def session_fixture(request, mocker):
+    def _patcher(module):
+        mocker.patch(f"{module}.get_app_data_path", return_value=Path("/tmp"))
 
-# Mock file data
-MOCK_FILE_ONE_DATA = os.urandom(1024)
-MOCK_FILE_TWO_DATA = os.urandom(1024)
-MOCK_FILE_THREE_DATA = os.urandom(1024)
-MOCK_FILE_FOUR_DATA = os.urandom(1024)
-MOCK_VALID_FILES = [
-    {"file": "pytorch_model.bin", "data": os.urandom(1024)},
-    {"file": "config.json", "data": os.urandom(1024)},
-]
-MOCK_FILES = [
-    {"file_path": "file_one.bin", "data": os.urandom(1024)},
-    {"file_path": "file_two.bin", "data": os.urandom(1024)},
-    {"file_path": "sub_folder/file_three.bin", "data": os.urandom(1024)},
-]
+    yield _patcher
+
+
+@pytest_asyncio.fixture(autouse=True)
+async def test_fixture(request):
+    clear_path()
+    await clear_db()
+    os.makedirs("/tmp/models", exist_ok=True)
+
+    async with init_state():
+        yield
+
+    def teardown():
+        asyncio.run(clear_db())
+        clear_path()
+
+    request.addfinalizer(teardown)
+
+
+@pytest.fixture
+def is_windows(mocker):
+    platform_module = sys.modules["platform"]
+    mocker.patch.object(platform_module, "system", return_value="Windows")
+
+
+@pytest.fixture
+def is_linux(mocker):
+    platform_module = sys.modules["platform"]
+    mocker.patch.object(platform_module, "system", return_value="Linux")
+
+
+@pytest.fixture
+def is_macos(mocker):
+    platform_module = sys.modules["platform"]
+    mocker.patch.object(platform_module, "system", return_value="Darwin")
+
+
+@pytest.fixture(autouse=True)
+def request_mocks(request, mocker):
+    with aioresponses() as mocked:
+        # HuggingFace
+        mocked.get(data.HF_API_URL, status=200, payload=data.MOCK_API_RESPONSE, repeat=True)
+        mocked.get(data.HF_API_URL_1, status=200, payload=data.mocked_response_1, repeat=True)
+        mocked.get(data.HF_API_URL_2, status=200, payload=data.mocked_response_2, repeat=True)
+        mocked.get(data.FILE_ONE_URL, status=200, body=data.MOCK_FILE_ONE_DATA, repeat=True)
+        mocked.get(data.FILE_TWO_URL, status=200, body=data.MOCK_FILE_TWO_DATA, repeat=True)
+        mocked.get(data.FILE_THREE_URL, status=200, body=data.MOCK_FILE_THREE_DATA, repeat=True)
+        mocked.get(data.FILE_FOUR_URL, status=200, body=data.MOCK_FILE_FOUR_DATA, repeat=True)
+
+        # Truffle
+        mocked.get(
+            f"{TRUFFLE_API_URL}/models?id={data.ID}&filter=id,name,title,size,author,downloads,likes,intro,capabilities,risks,evalId,hfLink,bg_image_url",
+            status=200,
+            payload=data.MOCK_MODEL_1,
+            repeat=True,
+        )
+        mocked.get(
+            f"{TRUFFLE_API_URL}/models?id={data.ID_2}&filter=id,name,title,size,author,downloads,likes,intro,capabilities,risks,evalId,hfLink,bg_image_url",
+            status=200,
+            payload=data.MOCK_MODEL_2,
+            repeat=True,
+        )
+        mocked.get(
+            f"{TRUFFLE_API_URL}/models/trending?k=5&filter=id,name,title,size,author,downloads,likes,intro,capabilities,risks,evalId,hfLink,bg_image_url",
+            status=200,
+            payload=data.MODELS,
+            repeat=True,
+        )
+        mocked.get(
+            f"{TRUFFLE_API_URL}/models/trending?k=4&filter=id,name,title,size,author,downloads,likes,intro,capabilities,risks,evalId,hfLink,bg_image_url",
+            status=200,
+            payload=data.MODELS[1:],
+            repeat=True,
+        )
+        mocked.get(
+            f"{TRUFFLE_API_URL}/models?id=3fec7228-04de-485d-9f09-bde6e8ea350f&filter=id,name,title,size,author,downloads,likes,intro,capabilities,risks,evalId,hfLink,bg_image_url",
+            status=200,
+            payload=data.MODELS[0],
+            repeat=True,
+        )
+
+        mock_head = mocker.patch("aiohttp.ClientSession.head")
+        mock_head.return_value.__aenter__.return_value = MagicMock(headers={"Content-Length": 1024})
+
+        yield mocked

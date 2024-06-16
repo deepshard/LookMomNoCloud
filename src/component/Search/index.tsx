54a1653656f308e2103897a19793170510ef9960
gbengaoyeee <oyesanya_ayobami@hotmail.com>
2024-06-16 12:35:16 -0700

    made a small change

--- src/component/Search/index.tsx
+++ src/component/Search/index.tsx
@@ -37,22 +37,30 @@ const Search = ({ onClose, recentlyUsedModels }: SearchProps) => {
     <div className="search">
       <img src="/assets/icons/close.svg" alt="" className="absolute cursor-pointer p-[10px] top-[20px] right-[20px]" onClick={onClose} />
       <div className="w-full mt-[131px] px-[145px]">
-        <Input onChange={(e) => setSearch(e.target.value)} placeholder="Search..." className="h-[38px] bg-transparent text-[32px] border-none" />
-        {isLoading ? (
-          <>loading...</>
+        <Input autoFocus onChange={(e) => setSearch(e.target.value)} placeholder="Search..." className="h-[38px] bg-transparent text-[32px] border-none" />
+        {search.length < 1 ? (
+          <>
+            <div className="flex justify-between mt-[52px]">
+              <Featured className="w-[303px] h-[150px] widget-3d" />
+              <Featured className="w-[303px] h-[150px] widget-3d" />
+            </div>
+            <div className="flex justify-between mt-[42px]">{recentlyUsedModels?.slice(0, 4).map((model) => <ModelWidget model={model} key={model.id} className="w-[124px] h-[78px]" />)}</div>
+          </>
         ) : (
           <>
-            {searchModels ? (
-              <>
-                <div className="flex justify-between mt-[42px]">{recentlyUsedModels?.slice(0, 4).map((model) => <ModelWidget model={model} key={model.id} className="w-[124px] h-[78px]" />)}</div>
-              </>
+            {isLoading ? (
+              <>loading...</>
             ) : (
               <>
-                <div className="flex justify-between mt-[52px]">
-                  <Featured className="w-[303px] h-[150px] widget-3d" />
-                  <Featured className="w-[303px] h-[150px] widget-3d" />
-                </div>
-                <div className="flex justify-between mt-[42px]">{recentlyUsedModels?.slice(0, 4).map((model) => <ModelWidget model={model} key={model.id} className="w-[124px] h-[78px]" />)}</div>
+                {searchModels ? (
+                  <>
+                    <div className="flex justify-between mt-[42px]">{searchModels?.slice(0, 8).map((model) => <ModelWidget model={model} key={model.id} className="w-[124px] h-[78px]" />)}</div>
+                  </>
+                ) : (
+                  <p>
+                    No results
+                  </p>
+                )}
               </>
             )}
           </>

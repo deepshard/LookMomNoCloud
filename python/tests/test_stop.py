3bf827185582492454a294c3c2b36f103393e5ef
Lienid <0xLienid@protonmail.com>
2024-06-17 11:07:14 -0700

    parametrize tests across os mocks

--- python/tests/test_stop.py
+++ python/tests/test_stop.py
@@ -1,6 +1,6 @@
 import pytest
 import asyncio
-from multiprocessing import Process, set_start_method
+from multiprocessing import Process, set_start_method, get_start_method
 
 from sqlalchemy import select
 from state import global_state_manager
@@ -23,7 +23,9 @@ async def fake_process():
 
 @pytest.fixture
 def mock_process():
-    set_start_method("spawn")
+    start_method = get_start_method()
+    if start_method != "spawn":
+        set_start_method("spawn")
     proc = Process(target=fake_process)
     proc.start()
     yield proc

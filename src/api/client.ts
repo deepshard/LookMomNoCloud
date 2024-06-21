8427de01d5fcabf1d139a0755f906abdab15cc29
gbengaoyeee <oyesanya_ayobami@hotmail.com>
2024-06-20 21:43:03 -0700

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,6 @@ import axios, { AxiosInstance } from 'axios'
 
 export const LOCAL_ROOT_URL = 'http://0.0.0.0:8899';
 const ROOT_URL = 'https://api.itsalltruffles.com';
-const API_TOKEN = "wEZY6SMz/uSI4THocY1GB4vwQP5+2RfKrDdQT5SbXP4="
 
 
 export class ApiClient {
@@ -19,7 +18,7 @@ export class ApiClient {
       baseURL: `${ROOT_URL}/${basePath}` ?? '/',
       responseType: 'json',
       headers: {
-        'Authorization': `Bearer ${API_TOKEN}`
+
       }
     });
   }

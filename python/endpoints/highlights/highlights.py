6a64af540c33f4410f10c3b21ee1d13b04373e42
Lienid <0xLienid@protonmail.com>
2024-06-23 15:31:28 -0700

    fix conflicts

--- python/endpoints/highlights/highlights.py
+++ python/endpoints/highlights/highlights.py
@@ -50,7 +50,7 @@ async def get_highlights() -> list[Model]:
         results.extend(model for model in trending_models if model.id not in all_models)
         results = results[:5]
 
-    return results[:5]
+    return results
 
 
 async def get_trending_models(num: int) -> list[Model]:

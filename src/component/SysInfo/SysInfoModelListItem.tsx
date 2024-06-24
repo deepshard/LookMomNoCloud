3c8a9845623dc564f0476c49c5ce6f7719e8ad3a
gbengaoyeee <oyesanya_ayobami@hotmail.com>
2024-06-23 18:55:35 -0700

    Update SysInfoModelListItem.tsx (#103)

--- src/component/SysInfo/SysInfoModelListItem.tsx
+++ src/component/SysInfo/SysInfoModelListItem.tsx
@@ -17,7 +17,7 @@ function SysInfoModelListItem({ model, selection = "memory", total }: SysInfoMod
     <>
       {model?.title && (
         <div className="sysinfo-model-item">
-          <img src="/src/assets/images/llama1.png" alt="" className="w-[30px] h-[30px] rounded-xs z-10" />
+          <img src={model.backgroundImage} alt="" className="w-[30px] h-[30px] rounded-xs z-10 object-contain" />
 
           <div className="flex flex-col justify-center items-start grow z-10">
             <p className="text-surface-750 title-sm">{model.title}</p>

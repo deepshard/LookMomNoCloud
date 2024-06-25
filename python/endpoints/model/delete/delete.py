18e2ff9490dcf40a9d3ff5a012ab950022b2c5b3
rk <this.rajkhare@gmail.com>
2024-06-25 14:37:20 -0700

    fix

--- python/endpoints/model/delete/delete.py
+++ python/endpoints/model/delete/delete.py
@@ -25,7 +25,4 @@ async def delete_model_handler(model_id: str):
 
     model_path = get_app_data_path() / "models" / model_id
     logger.info(f"Deleting model at: {model_path}")
-    try:
-        shutil.rmtree(model_path)
-    except FileNotFoundError:
-        pass
+    shutil.rmtree(model_path)

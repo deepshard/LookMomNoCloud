6152f1d7190a3bea99214ffdb279a23306c330b4
gbengaoyeee <oyesanya_ayobami@hotmail.com>
2024-06-26 00:04:41 -0700

    done no results view (#123)
    * done no results view
    
    * chnaged some input styling stuff

--- src/api/model.ts
+++ src/api/model.ts
@@ -120,7 +120,7 @@ export const getMyModels = async (): Promise<TModel[]> => {
 
 /** Remote API Calls */
 
-export const searchModels = async (query: string) => {
+export const searchModels = async (query: string): Promise<TModel[]> => {
   const response = await client.get(`/search/?query=${query}`)
   return response.data;
 }

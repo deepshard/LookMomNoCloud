ba12e96218d6cd313a28c9ae23c48fde154ed3b5
Raj Khare <37476411+raj-khare@users.noreply.github.com>
2024-06-26 13:53:24 -0700

    Bugfixes (#126)
    * fix
    
    * patch
    
    * fix
    
    * exclude readline -- it causes segfaults on ubuntu
    
    * comma?
    
    * test
    
    * logs
    
    * logd
    
    * FileNotFoundError
    
    * fix
    
    * cleanup
    
    * add version
    
    * lint
    
    * fix
    
    * fix
    
    * fix
    
    * bug
    
    * fix
    
    * fix integration tests (#128)
    
    Co-authored-by: Raj Khare <37476411+raj-khare@users.noreply.github.com>
    
    * frontend ci
    
    * log more stuff
    
    * slack
    
    * more bugs
    
    * fix
    
    * simplify
    
    * fix
    
    * new build
    
    * fix
    
    * spawn bug
    
    * Update InitializationProgress.tsx
    
    ---------
    
    Co-authored-by: 0xLienid <92545857+0xLienid@users.noreply.github.com>
    Co-authored-by: gbengaoyeee <oyesanya_ayobami@hotmail.com>

--- src/pages/OTA/InitializationProgress.tsx
+++ src/pages/OTA/InitializationProgress.tsx
@@ -18,7 +18,7 @@ const InitializationProgress = () => {
       if (value.progress === 1) {
         setTimeout(() => {
           navigate("/");
-        }, 2500)
+        }, 2500);
       }
     };
 
@@ -45,34 +45,30 @@ const InitializationProgress = () => {
 
   return (
     <AnimatePresence>
-      {
-        percent == 100 ? (
-          <motion.div initial={{ opacity: 0, x: 100 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -100 }} className="flex-center flex-col w-[271px]">
-            <p className="text-surface-500 text-center text-sm">All set</p>
-            <p className="text-white text-center text-lg mb-[66px]">Welcome to Truffle®</p>
-          </motion.div>
-        ) : (
-          <motion.div initial={{ opacity: 0, x: 100 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -100 }} className="flex-center flex-col w-[271px]">
-            <p className="text-white text-center text-lg mb-[66px]">Welcome to Truffle®</p>
-            <img src={truffleHardwareLandscapeIcon} alt="" className="w-[327px] h-[187px] blur-[0.4px] mb-[66px]" />
-            {
-              error ? (
-                <div className="flex justify-center items-center gap-2">
-                  <img src={errorIcon} alt="" />
-                  <p className="text-white text-center text-sm">An error occurred. Please restart the app</p>
-                </div>
-              ) : (
-                <>
-                  <Progress className="mb-[73px]" type="line" percent={percent} showInfo={false} />
-                  <p className="text-surface-500 text-center text-sm">Checking computer's firmware...</p>
-                </>
-              )
-            }
-          </motion.div>
-        )
-      }
+      {percent == 100 ? (
+        <motion.div initial={{ opacity: 0, x: 100 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -100 }} className="flex-center flex-col w-[271px]">
+          <p className="text-surface-500 text-center text-sm">All set</p>
+          <p className="text-white text-center text-lg mb-[66px]">Welcome to Truffle®</p>
+        </motion.div>
+      ) : (
+        <motion.div initial={{ opacity: 0, x: 100 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -100 }} className="flex-center flex-col w-[271px]">
+          <p className="text-white text-center text-lg mb-[66px]">Welcome to Truffle®</p>
+          <img src={truffleHardwareLandscapeIcon} alt="" className="w-[327px] h-[187px] blur-[0.4px] mb-[66px]" />
+          {error ? (
+            <div className="flex justify-center items-center gap-2">
+              <img src={errorIcon} alt="" />
+              <p className="text-white text-center text-sm">An error occurred. Please restart the app</p>
+            </div>
+          ) : (
+            <>
+              <Progress className="mb-[73px]" type="line" percent={percent} showInfo={false} />
+              <p>{percent}%</p>
+              <p className="text-surface-500 text-center text-sm">Checking computer's firmware...</p>
+            </>
+          )}
+        </motion.div>
+      )}
     </AnimatePresence>
-
   );
 };
 

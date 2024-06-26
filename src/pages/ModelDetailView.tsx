8b13142f8021e3806b79fa57ce5896f0ff845494
Aarya <63528145+gd3kr@users.noreply.github.com>
2024-06-26 14:21:34 -0700

    Added prev Srikanth commit

--- src/pages/ModelDetailView.tsx
+++ src/pages/ModelDetailView.tsx
@@ -11,26 +11,6 @@ import { useAppStore } from "../store/store";
 import { upperFirst } from "lodash";
 import { CircularProgressbar } from "react-circular-progressbar";
 import { useAppWrapper } from "../context/AppWrapperProvider";
-import {LazyLoadImage} from "react-lazy-load-image-component"
-import "react-lazy-load-image-component/src/effects/blur.css";
-// @ts-ignore
-import installIcon from "../assets/icons/install.svg";
-// @ts-ignore
-import stopIcon from "../assets/icons/stop.svg";
-// @ts-ignore
-import playIcon from "../assets/icons/play.svg";
-// @ts-ignore
-import shareIcon from "../assets/icons/share.svg";
-// @ts-ignore
-import trashIcon from "../assets/icons/trash.svg";
-// @ts-ignore
-import downloadIcon from "../assets/icons/download.svg";
-// @ts-ignore
-import closeIcon from "../assets/icons/close.svg";
-// @ts-ignore
-import downloadCircleIcon from "../assets/icons/download-circle-fill.svg";
-// @ts-ignore
-import likeCircleIcon from "../assets/icons/like-circle-fill.svg";
 
 function ModelDetailView() {
   const navBarOptions: NavBarOptions[] = ["intro", "capabilities", "risks", "evals"];
@@ -91,25 +71,27 @@ function ModelDetailView() {
           <div className="absolute top-0 left-0 p-4 flex w-full h-full justify-between">
             <div className="running-info flex justify-center items-center px-3 py-1 rounded-sm w-[173px] h-[32px]">
               <img src={"/src/assets/icons/running-man.svg"} className="mr-2 w-[16px] h-[16px]" />
-              <p className="text-surface-500 text-xs">https://localhost:{modelData.port}</p>
+              <p className="text-surface-500 text-xs">
+                https://localhost:{modelData.port}
+              </p>
             </div>
-            <div
-              onClick={() => window.open(`https://google.com/search?q=${modelData.name}`)}
-              className="running-info flex justify-center items-center px-3 py-1 rounded-sm w-[72px] h-[32px] cursor-pointer">
+            <div onClick={() => window.open(`https://google.com/search?q=${modelData.name}`)} className="running-info flex justify-center items-center px-3 py-1 rounded-sm w-[72px] h-[32px] cursor-pointer">
               <img src={"/src/assets/icons/docs.svg"} className="mr-2 w-[16px] h-[16px]" />
-              <p className="text-surface-500 text-xs">Docs</p>
+              <p className="text-surface-500 text-xs">
+                Docs
+              </p>
             </div>
           </div>
         );
       default:
         break;
     }
-  };
+  }
 
   const getModelStatusIcon = () => {
     switch (modelData?.status) {
       case "ACKNOWLEDGED":
-        return <Icon src={installIcon} imgClassName="h-full w-full animate-spin" />;
+        return <Icon src="/src/assets/icons/install.svg" imgClassName="h-full w-full animate-spin" />;
 
       case "DOWNLOADING":
         return (
@@ -129,12 +111,12 @@ function ModelDetailView() {
         );
 
       case "INSTALLING":
-        return <Icon src={installIcon} imgClassName="h-full w-full animate-spin" />;
+        return <Icon src="/src/assets/icons/install.svg" imgClassName="h-full w-full animate-spin" />;
 
       case "RUNNING":
         return (
           <Icon
-            src={stopIcon}
+            src="/src/assets/icons/stop.svg"
             imgClassName="h-[11px] w-[11px]"
             onClick={() => {
               stopModel(modelData).then((_) => {
@@ -150,7 +132,7 @@ function ModelDetailView() {
       case "STOPPED":
         return (
           <Icon
-            src={playIcon}
+            src="/src/assets/icons/play.svg"
             imgClassName="h-[11px] w-[11px]"
             onClick={() =>
               runModels([modelData], undefined, (updatedModel, controller) => {
@@ -171,139 +153,148 @@ function ModelDetailView() {
   };
 
   return (
-    <div className="absolute top-0 left-0 w-full h-full bg-bg-wdget-active">
-        <div className="model-detail-navbar">
-          <div className="w-1/4 flex gap-2.5 justify-start items-center z-[10]">
-            <img loading="lazy" srcSet={modelData?.backgroundImage} className="shrink-0 aspect-square rounded-full w-[30px] " />
+    <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-start items-center bg-black overflow-auto hide-scrollbar ">
+      {/* Nav Bar */}
+      <div className="sticky top-0 w-full p-5 gap-5 flex justify-between items-center z-[1000] mb-6">
+        { /* Left Side – Back Icon */}
+        <div className="w-1/4"></div>
 
-            <div className="flex flex-col justify-center items-start gap-0.5">
-              <p className="text-surface-main">{modelData && modelData?.name ? modelData.name.split("/")[1] : ""}</p>
-              <p className="text-surface-500 callout-base">{modelData && modelData?.author ? modelData.author : ""}</p>
+        {/* Center – Model Info Options*/}
+        <div className="flex items-center gap-3 text-surface-500 z-[1200] transition-colors duration-200 cursor-pointer">
+          {navBarOptions.map((item, index) => {
+            const displayTitle = item === "intro" ? "Introduction" : upperFirst(item);
+            return modelData && modelData[item] ? (
+              <a key={index} onClick={() => scrollToSection(item)} className="hover:text-white transition-colors duration-200">
+                {displayTitle}
+              </a>
+            ) : null;
+          })}
+        </div>
+
+        {/* Right Side – Icons */}
+        <div className="w-1/4 flex gap-2 justify-end items-center z-[999] cursor-pointer">
+          {modelData?.status && modelData?.status !== "NOT_DOWNLOADED" ? (
+            <>
+              {getModelStatusIcon()}
+
+              {/* Share Icon */}
+              <Icon src="/src/assets/icons/share.svg" imgClassName="h-[11px] w-[11px]" />
+
+              {/* Remove Icon */}
+              <Icon
+                src="/src/assets/icons/trash.svg"
+                imgClassName="h-[11px] w-[11px]"
+                onClick={() => {
+                  deleteModel(modelData).then((_) => {
+                    onDeleteModel(modelData);
+                    getMyModels();
+                    getHighlights();
+                  });
+                }}
+              />
+            </>
+          ) : (
+            <Icon
+              src="/src/assets/icons/download.svg"
+              imgClassName="h-[11px] w-[11px]"
+              className="w-auto flex-center gap-2 px-[24px] text-white"
+              onClick={() => {
+                modelData &&
+                  installModel(modelData, undefined, (progress) => {
+                    updateModels({
+                      ...modelData,
+                      ...progress,
+                    });
+                  });
+              }}>
+              <p className="text-sm">Install</p>
+            </Icon>
+          )}
+
+          {/* Close Icon */}
+          <Icon src="/src/assets/icons/close.svg" imgClassName="h-[11px] w-[11px]" onClick={() => window.history.back()} />
+        </div>
+      </div>
+
+      {/* Main Section – 100VH */}
+      <section className={"h-[100vh] max-w-[660px] mb-10 "}>
+        <div className="w-full h-full flex flex-col items-center space-y-auto">
+          <div className="relative flex flex-col justify-start items-center">
+            {/* Model's Image */}
+            <div className="w-[660px] h-[408px] rounded-2xl overflow-hidden glass-3d-no-blur">
+              {getModelInfoHeader()}
+              <img src={modelData?.backgroundImage} className=" w-full h-full " />
+            </div>
+
+            {/* Model's Name */}
+            <div className=" -bottom-10 flex flex-col items-start gap-0.5 p-6">
+              <p className="heading-md text-surface-main ">{modelData?.name.split("/")[1]}</p>
             </div>
           </div>
 
-          <div className="flex items-center gap-3 text-surface-500 z-[1200] transition-colors duration-200">
-            {navBarOptions.map((item, index) => {
-              const displayTitle = item === "intro" ? "Introduction" : upperFirst(item);
-              return modelData && modelData[item] ? (
-                <a key={index} onClick={() => scrollToSection(item)} className="hover:text-white transition-colors duration-200">
-                  {displayTitle}
-                </a>
-              ) : null;
-            })}
-          </div>
-
-          <div className="w-1/4 flex gap-2 justify-end items-center z-[999]">
-            {modelData?.status && modelData?.status !== "NOT_DOWNLOADED" ? (
-              <>
-                {getModelStatusIcon()}
-
-                <Icon src={shareIcon} imgClassName="h-[11px] w-[11px]" />
-
-                <Icon
-                  src={trashIcon}
-                  imgClassName="h-[11px] w-[11px]"
-                  onClick={() => {
-                    deleteModel(modelData).then((_) => {
-                      onDeleteModel(modelData);
-                      getMyModels();
-                      getHighlights();
-                    });
-                  }}
-                />
-              </>
-            ) : (
-              <Icon
-                src={downloadIcon}
-                imgClassName="h-[11px] w-[11px]"
-                className="w-auto flex-center gap-2 px-[24px] text-white"
-                onClick={() => {
-                  modelData &&
-                    installModel(modelData, undefined, (progress) => {
-                      updateModels({
-                        ...modelData,
-                        ...progress,
-                      });
-                    });
-                }}>
-                <p className="text-sm">Install</p>
-              </Icon>
+          {/* Model's Info */}
+          <div className="flex flex-col  items-center gap-5">
+            {modelData?.createdAt && modelData?.modifiedAt && (
+              <p className="text-surface-500">
+                Created {formatDate(modelData?.createdAt)} • Last Modified {formatDate(modelData?.modifiedAt)}
+              </p>
             )}
+            <div className="flex gap-2.5 text-sm text-white text-opacity-80">
+              {/* Author Tag */}
+              <Tag imgSrc="/src/assets/icons/author.svg" text={modelData?.author || ""} />
+              {/* Size Tag */}
+              <Tag imgSrc="/src/assets/icons/modelsize.svg" text={formatParams(modelData?.size)} />
 
-            <Icon src={closeIcon} imgClassName="h-[11px] w-[11px]" onClick={() => window.history.back()} />
+              {/* Downloads Tag */}
+              <Tag imgSrc="/src/assets/icons/download-circle-fill.svg" text={formatParams(modelData?.downloads)} />
+
+              {/* Likes/Bookmarks Tag */}
+              <Tag imgSrc="/src/assets/icons/like-circle-fill.svg" text={formatParams(modelData?.likes)} />
+            </div>
           </div>
         </div>
-      <div className="model-detail-view hide-scrollbar">
+      </section>
 
-        <section className={"h-[100vh] max-w-[660px] mb-10 "}>
-          <div className="w-full h-full flex flex-col items-center space-y-auto">
-            <div className="relative flex flex-col justify-start items-center">
-              <div className="w-[660px] h-[408px] rounded-2xl overflow-hidden glass-3d-no-blur">
-                {getModelInfoHeader()}
-                <LazyLoadImage effect="blur" src={modelData?.backgroundImage} className=" w-full h-full scale-110 " />
-              </div>
-
-              <div className=" -bottom-10 flex flex-col items-start gap-0.5 p-6">
-                <p className="heading-md text-surface-main ">{modelData?.name.split("/")[1]}</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col  items-center gap-5">
-              {modelData?.createdAt && modelData?.modifiedAt && (
-                <p className="text-surface-500">
-                  Created {formatDate(modelData?.createdAt)} • Last Modified {formatDate(modelData?.modifiedAt)}
-                </p>
-              )}
-              <div className="flex gap-2.5 text-sm text-white text-opacity-80">
-                <Tag text={modelData?.author || ""} />
-                <Tag text={formatParams(modelData?.size)} />
-
-                <Tag imgSrc={downloadCircleIcon} text={formatParams(modelData?.downloads)} />
-
-                <Tag imgSrc={likeCircleIcon} text={formatParams(modelData?.likes)} />
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section className="pt-16 pb-14">
-          <div>
-            {modelData && (
-              <div className="w-[660px] flex flex-col justify-start items-start gap-5">
-                {navBarOptions.map((section) => {
-                  if (modelData[section] && modelData[section] !== "") {
-                    let sectionRef;
-                    const displayTitle = section === "intro" ? "Introduction" : upperFirst(section);
-                    switch (section) {
-                      case "intro":
-                        sectionRef = introRef;
-                        break;
-                      case "capabilities":
-                        sectionRef = capabilitiesRef;
-                        break;
-                      case "risks":
-                        sectionRef = risksRef;
-                        break;
-                      case "evals":
-                        sectionRef = evalsRef;
-                        break;
-                      default:
-                        break;
-                    }
-                    return (
-                      <div key={section} ref={sectionRef}>
-                        <p className="title-base text-surface-500">{displayTitle}</p>
-                        <p className="text-surface-main body-long">{modelData[section]}</p>
-                      </div>
-                    );
+      <section className="pt-16 pb-14">
+        <div>
+          {/* Map with all sections – Limitations, Capabilities, Risks, Evals, etc */}
+          {modelData && (
+            <div className="w-[660px] flex flex-col justify-start items-start gap-5">
+              {navBarOptions.map((section) => {
+                if (modelData[section] && modelData[section] !== "") {
+                  let sectionRef;
+                  const displayTitle = section === "intro" ? "Introduction" : upperFirst(section);
+                  switch (section) {
+                    case "intro":
+                      sectionRef = introRef;
+                      break;
+                    case "capabilities":
+                      sectionRef = capabilitiesRef;
+                      break;
+                    case "risks":
+                      sectionRef = risksRef;
+                      break;
+                    case "evals":
+                      sectionRef = evalsRef;
+                      break;
+                    default:
+                      break;
                   }
-                  return null;
-                })}
-              </div>
-            )}
-          </div>
-        </section>
-      </div>
+                  return (
+                    <div key={section} ref={sectionRef}>
+                      {/* Section Title */}
+                      <p className="title-base text-surface-500">{displayTitle}</p>
+                      {/* Section Text */}
+                      <p className="text-surface-main body-long">{modelData[section]}</p>
+                    </div>
+                  );
+                }
+                return null;
+              })}
+            </div>
+          )}
+        </div>
+      </section>
     </div>
   );
 }

6dd3d82e3ba96214389222af481cdff267cf9035
Lienid <0xLienid@protonmail.com>
2024-06-26 12:20:17 -0700

    align window action buttons to discover

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,7 @@ const createWindow = () => {
     width: 950,
     height: 690,
     titleBarStyle: "hidden",
+    trafficLightPosition: { x: 21, y: 21 },
     webPreferences: {
       devTools: true,
       nodeIntegration: true,

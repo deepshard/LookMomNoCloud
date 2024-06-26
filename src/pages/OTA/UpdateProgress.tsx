b13edbef7a9e2cc3442e02fbbef2e834bbfd63b4
gbengaoyeee <oyesanya_ayobami@hotmail.com>
2024-06-25 23:41:22 -0700

    added nochos request for progress bar

--- src/pages/OTA/UpdateProgress.tsx
+++ src/pages/OTA/UpdateProgress.tsx
@@ -1,12 +1,13 @@
 import { Progress } from "antd";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
 // @ts-ignore
 import truffleHardwareLandscapeIcon from "../../assets/icons/truffle-hardware-landscape.svg";
+// @ts-ignore
 import errorIcon from "../../assets/icons/error.svg";
 import Button from "../../component/common/Button";
-import { TModel, TSysInfo } from "src/types/schemas";
+import { TModel, TSysInfo } from "../../types/schemas";
 import { stopModel } from "../../api/model";
 
 interface UpdateProgressProps {
@@ -65,20 +66,17 @@ const UpdateProgress = ({ sysInfo, updateModels }: UpdateProgressProps) => {
   return (
     <motion.div initial={{ opacity: 0, x: 100 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -100 }} className="flex-center flex-col w-[271px]">
       <img src={truffleHardwareLandscapeIcon} alt="" className="w-[327px] h-[187px] blur-[0.4px]" />
-      {
-        error ? (
-          <div className="flex justify-center items-center gap-2 mt-[81px]">
-            <img src={errorIcon} alt="" />
-            <p className="text-white text-center text-sm">An error occurred. Please restart the app</p>
-
-          </div>
-        ) : (
-          <>
-            <p className="text-white text-center text-sm mt-[81px]">Updating Truffle...</p>
-            <Progress className="mt-[30px]" type="line" percent={percent} showInfo={false} />
-          </>
-        )
-      }
+      {error ? (
+        <div className="flex justify-center items-center gap-2 mt-[81px]">
+          <img src={errorIcon} alt="" />
+          <p className="text-white text-center text-sm">An error occurred. Please restart the app</p>
+        </div>
+      ) : (
+        <>
+          <p className="text-white text-center text-sm mt-[81px]">Updating Truffle...</p>
+          {percent < 100 && <Progress className="mt-[30px]" type="line" percent={percent} showInfo={false} />}
+        </>
+      )}
       {percent === 100 && (
         <div className="flex-center flex-col mt-[30px] gap-2">
           <p>Restart is required to complete the update</p>

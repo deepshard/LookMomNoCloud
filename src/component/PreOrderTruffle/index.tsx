a73052beb203aae612a9e4017486102590e3439b
Nacho-Giorgis <nacho@getswype.io>
2024-06-26 23:52:14 -0300

    added hover: state

--- src/component/PreOrderTruffle/index.tsx
+++ src/component/PreOrderTruffle/index.tsx
@@ -8,7 +8,7 @@ const PreOrderTruffle = () => {
   };
 
   return (
-    <div className="flex flex-col rounded-lg overflow-hidden w-full h-full relative p-[17px] pt-[20px] pb-[20px]">
+    <div className="group transition ease-out duration-500 flex flex-col rounded-lg overflow-hidden w-full h-full relative p-[17px] pt-[20px] pb-[20px]">
       <div className="flex-grow flex justify-center items-center">
         <img
           src="/src/assets/images/truffle-device.png"
@@ -21,13 +21,13 @@ const PreOrderTruffle = () => {
           <p className="text-surface-500 text-xs">$1,499.00</p>
           <p className="text-surface-main text-xl">Truffle–1</p>
         </div>
-        <Button
-          className="w-full mt-2 !px-[10px] h-[37px] rounded-[13px] bg-surface-100 border-none glass-3d text-surface-main text-xs"
+        <button
+          className="h-0 group-hover:h-10 opacity-0 group-hover:opacity-100 transition ease-out duration-500 w-full mt-2 !px-2.5 rounded-sm bg-transparent  hover:bg-bg-wdget border-none glass-3d text-surface-main"
           onClick={handlePreOrder}
           style={{ borderRadius: '13px' }}
         >
-          Pre-order
-        </Button>
+          Pre-Order
+        </button>
       </div>
     </div>
   )

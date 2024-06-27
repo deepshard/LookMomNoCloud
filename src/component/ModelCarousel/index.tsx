2037dbf9192eec400f52ae7932e71bcdbb4bbe0d
gbengaoyeee <oyesanya_ayobami@hotmail.com>
2024-06-26 20:03:19 -0700

    done aligning shit to center

--- src/component/ModelCarousel/index.tsx
+++ src/component/ModelCarousel/index.tsx
@@ -8,7 +8,7 @@ import './index.css'
 import { useAppStore } from '../../store/store'
 import { canFitOnMachine } from '../../utils/sysUtils'
 
-interface ModelCarouselProps {
+interface ModelCarouselProps extends React.HTMLAttributes<HTMLDivElement> {
   models: TModel[]
   isLoading: boolean
   installModel: (...args: any) => void
@@ -37,7 +37,7 @@ const getSortValue = (status: string) => {
   }
 }
 
-const ModelCarousel: React.FC<ModelCarouselProps> = ({ models, isLoading, installModel, runModels, stopModel, cleanupInstall, onModelClick  }) => {
+const ModelCarousel: React.FC<ModelCarouselProps> = ({ models, isLoading, installModel, runModels, stopModel, cleanupInstall, onModelClick, className='', ...props  }) => {
   const skeletonCount = 5
   const { sysInfo } = useAppStore();
   const [showModels, setShowModels] = useState(false)
@@ -69,7 +69,7 @@ const ModelCarousel: React.FC<ModelCarouselProps> = ({ models, isLoading, instal
   }, [models])
 
   return (
-    <div className="model-carousel custom-scrollbar">
+    <div className={`model-carousel custom-scrollbar ${className}`} {...props}>
       <div className="model-carousel-inner">
         {(isLoading || !showModels) &&
           Array(skeletonCount)
--- src/main.ts
+++ src/main.ts
@@ -93,7 +93,7 @@ const createWindow = () => {
   // Open the DevTools.
   // mainWindow.setResizable(false);
 
-  if (process.env.NODE_ENV == "dev") {
+  if (process.env.NODE_ENV === "development") {
     mainWindow.webContents.openDevTools();
     mainWindow.setResizable(true);
   }

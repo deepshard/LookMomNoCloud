51d122d49432b1111616d9416c86060252495639
rk <this.rajkhare@gmail.com>
2024-06-28 13:11:47 -0700

    asd

--- src/app.tsx
+++ src/app.tsx
@@ -6,18 +6,25 @@ import QueryProvider from "./lib/react-query/QueryProvider";
 import ModelDetailView from "./pages/ModelDetailView";
 import AppWrapperProvider from "./context/AppWrapperProvider";
 import OTA from "./pages/OTA";
-import Analytics from "./types/Analytics";
+import mixpanel from "mixpanel-browser";
+import { v4 as uuidv4 } from 'uuid';
 
 // @ts-ignore
 const root = createRoot(document.getElementById("root"));
 
-Analytics.init("de5db316f13ff4b8fefdcd81cf8bd696");
+mixpanel.init('a45767d32d620a6ba48640ccec2bf2f3', { debug: true, track_pageview: true, persistence: 'localStorage' });
+let deviceId = localStorage.getItem("deviceId");
+if (!deviceId) {
+  deviceId = uuidv4();
+  localStorage.setItem("deviceId", deviceId);
+}
+mixpanel.identify(deviceId)
 
 function App() {
   return (
     <div>
       <div className="app-body">
-        <div className="clear-dotted-bg"/>
+        <div className="clear-dotted-bg" />
         <HashRouter>
           <QueryProvider>
             <AppWrapperProvider>

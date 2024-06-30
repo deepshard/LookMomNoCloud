e889881bd7aa840a241b147a8fad09399d8a42b7
rk <this.rajkhare@gmail.com>
2024-06-29 18:40:37 -0700

    fix

--- python/endpoints/model/run/run.py
+++ python/endpoints/model/run/run.py
@@ -1,3 +1,5 @@
+import contextlib
+import io
 import os
 import multiprocessing
 import asyncio
@@ -230,6 +232,23 @@ async def is_server_running(port: int, timeout: int = 120) -> bool:
     return False
 
 
+class AutoFlushFile(io.TextIOWrapper):
+    def __init__(self, f):
+        super().__init__(f, encoding='utf-8')
+
+    def write(self, s):
+        super().write(s)
+        self.flush()
+
+
+@contextlib.contextmanager
+def managed_file(filename):
+    file = open(filename, "a+b")
+    try:
+        yield AutoFlushFile(file)
+    finally:
+        file.close()
+        
 async def run_model(
     model_id: str, quantization: Quantization, mem_share: float, instance: int, port: int
 ) -> ProgressEvent:
@@ -246,25 +265,28 @@ async def run_model(
     # proc.start()
 
     mlc_llm_path = Path(sys._MEIPASS) / ".." / "mlc_llm_serve"
-    proc = subprocess.Popen(
-        [
-            mlc_llm_path,
-            model_path,
-            "--model-lib",
-            str(model_path / "compilation.so"),
-            "--port",
-            str(port),
-            "--host",
-            "0.0.0.0",
-            "--mode",
-            "interactive",
-            "--overrides",
-            f'"gpu_memory_utilization={mem_share};tensor_parallel_shards={shards}"',
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        start_new_session=True,
-    )
+    with managed_file("models.log") as auto_flush_file:
+        cmd = [
+                mlc_llm_path,
+                model_path,
+                "--model-lib",
+                str(model_path / "compilation.so"),
+                "--port",
+                str(port),
+                "--host",
+                "0.0.0.0",
+                "--mode",
+                "interactive",
+                "--overrides",
+                f"gpu_memory_utilization={mem_share};tensor_parallel_shards={shards}",
+        ]
+        logger.info(f"Running command: {cmd}")
+        proc = subprocess.Popen(
+            cmd,
+            stdout=auto_flush_file,
+            stderr=auto_flush_file,
+            start_new_session=True,
+        )
 
     # Wait for the server to start and be available
     server_ready = await is_server_running(port)

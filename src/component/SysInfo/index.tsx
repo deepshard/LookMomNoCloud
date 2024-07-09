7b78b6ea130304dffe1d42745b0dc445cf473bba
gbengaoyeee <oyesanya_ayobami@hotmail.com>
2024-07-09 15:47:19 -0700

    Update index.tsx

--- src/component/SysInfo/index.tsx
+++ src/component/SysInfo/index.tsx
@@ -126,7 +126,7 @@ const Sysinfo = ({ sysInfo }: SysInfoProps) => {
               onClick={() => onSelectionChange("disk")}
             />
           </span>
-          <span>{bytesToHumanReadable(getUsed(), false, 0)} of {bytesToHumanReadable(sysInfo.resources.total[selection === "memory" ? "ram" : "disk"], true, 0)}</span>
+          <span>{bytesToHumanReadable(getUsed(), false, 2)} of {bytesToHumanReadable(sysInfo.resources.total[selection === "memory" ? "ram" : "disk"], true, 0)}</span>
         </div>
       </div>
       <div className={`h-[${defaultProgressBarContainerHeight}px] mt-[40px] sticky top-[80px] w-full flex items-center px-8`}>
